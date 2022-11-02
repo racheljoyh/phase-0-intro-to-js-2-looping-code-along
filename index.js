4947ef64fdad6988c7cb3beb5170e5270381de66
Rachel <racheljhumes@gmail.com>
2022-11-01 20:25:38 -0600

    commit tree 0b35f68fd7899af072dd572976b118386486bd85

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < array.length; i++) {
+    thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouCards
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(num) {
+  while (num > 0 ) {
+    
+    console.log(num--)
+  }
+  console.log(num)
+}
+
+countDown(10)
